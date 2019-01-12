64e4d8a192e4616cf10e74367a7ab52ac6bb73fc
shivkore <kores9214@gmail.com>
2019-01-12 15:20:29 +0530

    Assignment10 added

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AlertModule.forRoot(),
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
